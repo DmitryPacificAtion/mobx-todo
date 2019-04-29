0799aa73140afffacf2a3637c0a4a318e02c6cc2
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2019-04-29 17:11:04 +0300

    Failure try to move onto webpack

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from "../components/todo";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="app">
+        <Todo/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/todo/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Todo extends React.Component {
+    public render() {
+        return (
+            <div className="todo">Todo</div>
+        );
+    }
+}
+
+export default Todo;
